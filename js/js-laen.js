612e3a3cc39500173d4ba03718210d7451830f69
Sir Joseph <sirjosephholmes@gmail.com>
2021-05-19 23:49:51 +0700

    tambah function ganti ikon expand menu n login

--- js/js-laen.js
+++ js/js-laen.js
@@ -5,10 +5,29 @@
 $("document").ready(function(){
     $(".gambar-artikel").css("background-image : url('../img/product/jennie-airport-fashion.jpg');");
     $("img").addClass("img-responsive");
+	});
+	
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
 /*==================================================================================
-    AKHIR ANIMASI NAVBAR PARALLAX
+    PERUBAHAN IKON KETIKA MENU LOGIN SINKRON DGN DB
 ====================================================================================*/
+
+	let statusLogin = false ;
+	function gantiLogin (satusLogin){
+			if (statusLogin === false){
+					$(".menu_login").html(" <span class="material-icons">perm_identity</span> ");
+				}
+			else {
+					$("menu_login").html("Log In")
+				}
+		}
+		
+	gantiLogin();
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
 
 /*==================================================================================
     contoh pembatas
 ====================================================================================*/
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
